caa9de994272c719cee97f6cf463d89ad232e526
Marin Kyouko <marinkyouko@gmail.com>
2023-01-16 11:31:24 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+    }
+}
